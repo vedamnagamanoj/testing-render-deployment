b4693723cebc91506fd390d590f729eb0258f6fd
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2025-07-12 15:18:46 +0530

    replaced baseURL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 function App() {
   return (
